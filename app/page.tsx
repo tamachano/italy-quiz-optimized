4e601bfbdbfb24a9aeba127fe1c69a06b3263f2e
tamachano <i.cclimber.luigi@gmail.com>
2026-06-01 09:35:23 +0900

    クイズの導入文を修正

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
       <div className="home-content">
       <h1>イタリア名所クイズ</h1>
       <p>
-        全てイタリア国内です。次の場所はどこでしょう！？
+        イタリアの風景を感じる5問のクイズです。次の場所はどこでしょう？
       </p>
       <Link href="/quiz">
   <span>ここをクリック！</span>
